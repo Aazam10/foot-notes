d5de80b192ac25d03a5782d37ec39be17bb74059
Aazam10 <aazam.hudda94@gmail.com>
2022-04-02 14:01:48 +0530

    added react router configured mockman

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import Mockman from "mockman-js";
 import { FaTags } from "react-icons/fa";
 
 function App() {
   return (
     <div>
       hi <FaTags />
+      <Routes>
+        <Route path="/mock" element={<Mockman />} />
+      </Routes>
     </div>
   );
 }
