55288e1bde529e2c2c7376b1cfe173ce7d2ae712
Aazam10 <aazam.hudda94@gmail.com>
2022-04-08 01:54:29 +0530

    fix:removed unwanted code

--- src/components/NoteList/NoteList.jsx
+++ src/components/NoteList/NoteList.jsx
@@ -2,8 +2,8 @@ import { useNotes } from "../../context";
 import { NoteCard } from "../NoteCard/NoteCard";
 const NoteList = () => {
   const { noteState } = useNotes();
-  const { notes, trash } = noteState;
-  console.log(notes, trash);
+  const { notes } = noteState;
+
   return (
     <div className="notes-card-container">
       {notes.map((note) => {
--- src/reducer/noteReducer.js
+++ src/reducer/noteReducer.js
@@ -1,8 +1,6 @@
 const noteInitialState = { notes: [], trash: [] };
 
 const noteReducer = (state, action) => {
-  console.log(state.trash);
-  console.log(action.payload.trash);
   switch (action.type) {
     case "ADD_NOTE":
       return { ...state, notes: action.payload };
