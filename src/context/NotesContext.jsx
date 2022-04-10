3e7e5b8a071603c6604ab4704052c954fc23cb45
Aazam10 <aazam.hudda94@gmail.com>
2022-04-11 00:20:47 +0530

    feat:updated context

--- src/context/NotesContext.jsx
+++ src/context/NotesContext.jsx
@@ -13,9 +13,29 @@ const NotesProvider = ({ children }) => {
   const [noteState, noteDispatch] = useReducer(noteReducer, noteInitialState);
   const [showNoteForm, setShowNoteForm] = useState(false);
 
+  const inputInitialState = {
+    title: "",
+    content: "",
+    bgcolor: "",
+    priority: "",
+  };
+
+  const [noteDetails, setNoteDetails] = useState(inputInitialState);
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <NotesContext.Provider
-      value={{ noteState, noteDispatch, showNoteForm, setShowNoteForm }}
+      value={{
+        noteState,
+        noteDispatch,
+        showNoteForm,
+        setShowNoteForm,
+        noteDetails,
+        setNoteDetails,
+        inputInitialState,
+        isEditing,
+        setIsEditing,
+      }}
     >
       {children}
     </NotesContext.Provider>
