91a660320d29ac1790821734f83e56aa64af7a8e
Aazam10 <aazam.hudda94@gmail.com>
2022-04-11 00:21:36 +0530

    feat: added priority

--- src/components/NoteCard/NoteCard.jsx
+++ src/components/NoteCard/NoteCard.jsx
@@ -6,9 +6,13 @@ import {
   MdDeleteForever,
   MdArchive,
   MdUnarchive,
+  MdEdit,
+  MdBarChart,
 } from "react-icons/md";
+import axios from "axios";
 import { useAuth, useNotes } from "../../context";
-
+import { ColorPallete } from "../ColoPallete/ColorPallete";
+import { Priority } from "../Priority/Priority";
 import {
   addToTrashFn,
   restoreFromTrashFn,
@@ -16,7 +20,10 @@ import {
   addToArchiveFn,
   restoreArchiveFn,
   addToTrashFromArchiveFn,
+  editNoteFn,
+  editArchiveNoteFn,
 } from "../../utils";
+import { useState } from "react";
 
 const NoteCard = ({ note }) => {
   const {
@@ -26,6 +33,9 @@ const NoteCard = ({ note }) => {
   const {
     noteState: { notes, trash, archives },
     noteDispatch,
+    setNoteDetails,
+    setShowNoteForm,
+    setIsEditing,
   } = useNotes();
 
   const addToTrashHandler = (id) => {
@@ -55,49 +65,101 @@ const NoteCard = ({ note }) => {
     (archivedNote) => archivedNote._id === note._id
   );
 
+  const [showColorPallete, setShowColorPallete] = useState(false);
+  const [showPriority, setShowPriority] = useState(false);
+
+  const updateNoteHandler = async (currentNote) => {
+    isInArchive
+      ? editArchiveNoteFn(currentNote, token, noteDispatch)
+      : editNoteFn(currentNote, token, noteDispatch);
+  };
+
+  const editIconClickHandler = () => {
+    setNoteDetails({ ...note });
+    setIsEditing(true);
+    setShowNoteForm(true);
+  };
+
+  const priorityIconClickHandler = () => {
+    setShowColorPallete(false);
+    setShowPriority((prev) => !prev);
+  };
+
   return (
-    <section className="note-card">
+    <section className="note-card" style={{ backgroundColor: note.bgcolor }}>
+      {note.priority !== "" ? (
+        <div className="priority-tag">{note.priority}</div>
+      ) : null}
       <h2 className="note-title">{note.title}</h2>
       <p className="note-description">{note.content}</p>
       <div className="note-card-footer">
-        <p>{note.createdDate}</p>
-        {isInTrash ? (
-          <div className="note-icons-container">
-            <MdRestoreFromTrash
-              className="icon"
-              onClick={() => restoreFromTrashClickHandler(note._id)}
-            />
-            <MdDeleteForever
-              className="icon"
-              onClick={() => deleteForeverClickHandler(note._id)}
-            />
-          </div>
-        ) : isInArchive ? (
-          <div className="note-icons-container">
-            <MdColorLens className="icon" />
-            <MdUnarchive
-              className="icon"
-              onClick={() => restoreFromArchiveClickHandler(note._id)}
-            />
-            <FaTrashAlt
-              className="icon"
-              onClick={() => addToTrashFromArchiveHandler(note._id)}
-            />
-          </div>
-        ) : (
-          <div className="note-icons-container">
-            <MdColorLens className="icon" />
-            <MdArchive
-              className="icon"
-              onClick={() => addToArchiveClickHandler(note._id)}
-            />
-            <FaTrashAlt
-              className="icon"
-              onClick={() => addToTrashHandler(note._id)}
-            />
-          </div>
-        )}
+        <p className="note-date">{note.createdDate}</p>
+        <div className="icon-wrapper">
+          {isInTrash ? (
+            <div className="note-icons-container">
+              <MdRestoreFromTrash
+                className="icon"
+                onClick={() => restoreFromTrashClickHandler(note._id)}
+              />
+              <MdDeleteForever
+                className="icon"
+                onClick={() => deleteForeverClickHandler(note._id)}
+              />
+            </div>
+          ) : isInArchive ? (
+            <div className="note-icons-container">
+              <MdEdit
+                className="icon"
+                onClick={() => editIconClickHandler(note._id)}
+              />
+              <MdBarChart className="icon" onClick={priorityIconClickHandler} />
+              <MdColorLens
+                className="icon"
+                onClick={() => setShowColorPallete((prev) => !prev)}
+              />
+              <MdUnarchive
+                className="icon"
+                onClick={() => restoreFromArchiveClickHandler(note._id)}
+              />
+              <FaTrashAlt
+                className="icon"
+                onClick={() => addToTrashFromArchiveHandler(note._id)}
+              />
+            </div>
+          ) : (
+            <div className="note-icons-container">
+              <MdEdit className="icon" onClick={editIconClickHandler} />
+              <MdBarChart className="icon" onClick={priorityIconClickHandler} />
+              <MdColorLens
+                className="icon"
+                onClick={() => setShowColorPallete((prev) => !prev)}
+              />
+              <MdArchive
+                className="icon"
+                onClick={() => addToArchiveClickHandler(note._id)}
+              />
+              <FaTrashAlt
+                className="icon"
+                onClick={() => addToTrashHandler(note._id)}
+              />
+            </div>
+          )}
+        </div>
       </div>
+      {showColorPallete && (
+        <ColorPallete
+          note={note}
+          updateNoteHandler={updateNoteHandler}
+          setShowColorPallete={setShowColorPallete}
+        />
+      )}
+      {showPriority && (
+        <Priority
+          note={note}
+          updateNoteHandler={updateNoteHandler}
+          setShowPriority={setShowPriority}
+        />
+      )}
     </section>
   );
 };
--- src/components/NoteForm/NoteForm.jsx
+++ src/components/NoteForm/NoteForm.jsx
@@ -1,23 +1,38 @@
 import "./NoteForm.css";
 import { FaTags } from "react-icons/fa";
-import { MdColorLens, MdCancel } from "react-icons/md";
-import { useState } from "react";
+import { MdColorLens, MdCancel, MdBarChart } from "react-icons/md";
+
 import { useAuth, useNotes } from "../../context";
 
 import { addNote } from "../../Services";
+import { editArchiveNoteFn, editNoteFn } from "../../utils";
+import { useState } from "react";
+import { ColorPallete } from "../ColoPallete/ColorPallete";
+import { Priority } from "../Priority/Priority";
 
 const NoteForm = () => {
   const { authState } = useAuth();
   const {
-    noteState: { notes },
+    noteState: { notes, archives },
     noteDispatch,
     showNoteForm,
     setShowNoteForm,
+    noteDetails,
+    setNoteDetails,
+    inputInitialState,
+    setIsEditing,
   } = useNotes();
 
   const { token } = authState;
 
-  const [noteDetails, setNoteDetails] = useState({ title: "", content: "" });
+  // const [noteDetails, setNoteDetails] = useState({ title: "", content: "" });
+  const [showColorPallete, setShowColorPallete] = useState(false);
+  const [showPriority, setShowPriority] = useState(false);
+
+  const noteExists = notes.find((note) => note._id === noteDetails._id);
+  const archiveExists = archives.find(
+    (archivedNote) => archivedNote._id === noteDetails._id
+  );
 
   const createdDate = new Date().toLocaleString();
 
@@ -26,33 +41,72 @@ const NoteForm = () => {
     setNoteDetails({ ...noteDetails, [name]: value });
   };
 
+  const closeNoteForm = () => {
+    setShowNoteForm(false);
+    setIsEditing(false);
+    setNoteDetails(inputInitialState);
+  };
+
   const submitNotebtnHandler = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await addNote(
-        { ...noteDetails, createdDate: createdDate },
-        token
+    if (noteExists) {
+      editNoteFn(
+        {
+          ...noteExists,
+          title: noteDetails.title.trim(),
+          content: noteDetails.content.trim(),
+          bgcolor: noteDetails.bgcolor,
+          priority: noteDetails.priority,
+        },
+        token,
+        noteDispatch
       );
-      if (response.status === 201) {
-        noteDispatch({ type: "ADD_NOTE", payload: response.data.notes });
-        setShowNoteForm(false);
+    } else if (archiveExists) {
+      editArchiveNoteFn(
+        {
+          ...archiveExists,
+          title: noteDetails.title.trim(),
+          content: noteDetails.content.trim(),
+          bgcolor: noteDetails.bgcolor,
+          priority: noteDetails.priority,
+        },
+        token,
+        noteDispatch
+      );
+    } else {
+      if (noteDetails.title.trim() === "") {
+        alert("title  should be added");
       } else {
-        throw new Error();
+        try {
+          const response = await addNote(
+            { ...noteDetails, createdDate: createdDate },
+            token
+          );
+          if (response.status === 201) {
+            noteDispatch({ type: "ADD_NOTE", payload: response.data.notes });
+          } else {
+            throw new Error();
+          }
+        } catch (error) {
+          alert(error);
+        }
       }
-    } catch (error) {
-      alert(error);
     }
+    closeNoteForm();
   };
-
-  const hideFormHandler = () => {
-    setShowNoteForm(false);
+  const priorityIconClickHandler = () => {
+    setShowColorPallete(false);
+    setShowPriority((prev) => !prev);
   };
 
   return (
     <div className="form-open">
-      <section className="note-form-container">
-        <MdCancel className="cancel-icon" onClick={hideFormHandler} />
+      <section
+        className="note-form-container"
+        style={{ backgroundColor: noteDetails.bgcolor }}
+      >
+        <MdCancel className="cancel-icon" onClick={closeNoteForm} />
         <form>
           <input
             type="text"
@@ -71,7 +125,11 @@ const NoteForm = () => {
           ></textarea>
           <div className="tools-container">
             <div className="tools">
-              <MdColorLens className="icon" />
+              <MdColorLens
+                className="icon"
+                onClick={() => setShowColorPallete((prev) => !prev)}
+              />
+              <MdBarChart className="icon" onClick={priorityIconClickHandler} />
               <FaTags className="icon" />
             </div>
 
@@ -80,10 +138,12 @@ const NoteForm = () => {
               type="submit"
               onClick={submitNotebtnHandler}
             >
-              Add Note
+              {noteExists || archiveExists ? "Save" : "Add"}
             </button>
           </div>
         </form>
+        {showColorPallete && <ColorPallete />}
+        {showPriority && <Priority />}
       </section>
     </div>
   );
