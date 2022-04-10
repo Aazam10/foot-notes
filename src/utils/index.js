645eef59beb7258846bc2b8aa0d3fa132d838377
Aazam10 <aazam.hudda94@gmail.com>
2022-04-11 00:20:03 +0530

    feat: edit note functions added

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,5 @@ export { deleteFromTrashFn } from "./Trash/deleteFromTrashFn";
 export { addToArchiveFn } from "./Archive/addToArchiveFn";
 export { restoreArchiveFn } from "./Archive/restoreArchiveFn";
 export { addToTrashFromArchiveFn } from "./Archive/addToTrashFromArchiveFn";
+export { editNoteFn } from "./Note/editNoteFn";
+export { editArchiveNoteFn } from "./Archive/editArchiveNoteFn";
